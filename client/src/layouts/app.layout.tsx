41eefb5e22b1b61bc242bede457ed8c521ea7b04
Tommacfie <tommacfie@aol.com>
2021-10-12 14:17:28 +0200

    feat(routing): added routing comments and redirects where needed

--- client/src/layouts/app.layout.tsx
+++ client/src/layouts/app.layout.tsx
@@ -3,46 +3,76 @@ import Dashboard from '../screen/Dashboard.screen';
 import SelectJob from '../screen/SelectJob.screen';
 import { useContext } from 'react';
 import { UserContext } from '../services/Context';
-import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import CreateJob from '../screen/CreateJob.screen';
+import NotFound from '../screen/NotFound.screen';
+
 
 const AppLayout = () => {
 
   const { user } = useContext(UserContext)
 
-
-
   return (
     <>
-      {/* {Customer Route} */}
+      {/* If user isn't authenticated, disable route rendering and redirect to login */}
+      {!user.isAuthenticated ?
 
-      {/* This will redirect to the login  page if the user logs out or becomes unauthenticated */}
-      {/* {!user.isAuthenticated && <Redirect to='/auth/login' />} */}
-      <Route path='/app/customer/selectjob'>
-        <h1>Customer Select Job (Home)</h1>
-        <SelectJob />
-      </Route>
+        //Customer Route
+        <Switch>
+          {/* Selectjob is where a customer can select a job type from a 3-button menu */}
+          <Route exact path='/app/customer/selectjob'>
+            <h1>Customer Select Job (Home)</h1>
+            <SelectJob />
+          </Route>
 
-      <Route path='/app/customer/dashboard'>
-        <h1>Customer Dashboard</h1>
-        <Dashboard />
-      </Route>
+          {/* Customer Dashboard is where the user can see a list of active jobs */}
+          <Route path='/app/customer/dashboard'>
+            <h1>Customer Dashboard</h1>
+            <Dashboard />
+          </Route>
 
-      <Route path='/app/customer/selectjob/espresso'>
-        {/* ESPRESSO JOB */}
-      </Route>
-      <Route path='/app/customer/selectjob/cappuccino'>
-        {/* CAPPUCINO JOB */}
-      </Route>
-      <Route path='/app/customer/selectjob/macchiato'>
-        {/* MACCHIATO JOB */}
-      </Route>
+          {/* This is the route for Espresso, the user will be taken to a screen where the image can be submitted */}
+          <Route exact path='/app/customer/createjob/espresso'>
+            {/* ESPRESSO CREATE JOB */}
+            <h1>ESPRESSO</h1>
+          </Route>
 
-      {/* {Translator Route} */}
-      <Route path='/app/translator/dashboard'>
-        <h1>Translator Dashboard (Home)</h1>
-        <Dashboard />
-      </Route>
+          {/* This is the route for Cappuccino, where a user will taken to a chat screen  */}
+          <Route exact path='/app/customer/createjob/cappuccino'>
+            {/* CAPPUCINO CREATE JOB */}
+            <h1>CAPPUCCINO</h1>
+          </Route>
+
+          {/* This is the route for Macchiato, where a user will taken to a video chat screen  */}
+          <Route exact path='/app/customer/createjob/macchiato'>
+            {/* MACCHIATO CREATE JOB */}
+            <h1>MACCHIATO</h1>
+          </Route>
+
+
+          {/* Translator Route */}
+          {/* This is the translator dashboard, where a translator can view available jobs for translation */}
+          <Route exact path='/app/translator/dashboard'>
+            <h1>Translator Dashboard (Home)</h1>
+            <Dashboard />
+          </Route>
+          {/* Here the translator can view pending and completed jobs  */}
+          <Route exact path='/app/translator/dashboard/history'>
+            <h1>Translator Accepted (Home)</h1>
+            <Dashboard />
+          </Route>
+
+
+          {/* 404 route */}
+          <Route path='*'>
+            <NotFound />
+          </Route>
+
+        </Switch>
+        :
+        // Unauthorised user sent straight to login
+        <Redirect to='/auth/login'></Redirect>
+      }
     </>
   )
 }
