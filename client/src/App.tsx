6f9959bc7c2696060d24c595d7c583fde37076fb
Francesco Elia Foradori <francesco.elia.foradori@gmail.com>
2021-10-12 15:56:20 +0200

    style(favicon.ico): added favicon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,27 @@
 import './App.scss';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import AuthLayout from './layouts/auth.layout';
 import AppLayout from './layouts/app.layout';
 import NotFound from './screen/NotFound.screen';
-import LoginScreen from './screen/Login.screen';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/auth'><AuthLayout /></Route>
-        <Route path='/app'><AppLayout /></Route>
-        <Redirect from='/' to='auth/login'><LoginScreen /></Redirect>
-        <Route path='*'><NotFound /></Route>
+        <Route path="/auth">
+          <AuthLayout />
+        </Route>
+        <Route path="/app">
+          <AppLayout />
+        </Route>
+        <Redirect path="*">
+          <NotFound />{' '}
+        </Redirect>
       </Switch>
     </Router>
   );
