253972a0ede85f1931fd0925aa5006df45323c77
Francesco Elia Foradori <francesco.elia.foradori@gmail.com>
2021-10-13 21:30:59 +0200

    feat: added functionality
    added functionality in order to manipolate text

--- client/src/components/image/Translator.image.tsx
+++ client/src/components/image/Translator.image.tsx
@@ -1,9 +1,12 @@
-import React /*,{ useState }*/ from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import * as markerjs2 from 'markerjs2';
 
 const TranslatorImage = (props: { job: any }) => {
-  const { jobName, imageUrl } = props.job;
+  const { jobName, imageUrl, translationText } = props.job;
+
+  const [value, setValue] = useState('');
+
   // needed if we want a confirmation img
   // const [resultsImage, setResultsImage] = useState('');
 
@@ -20,38 +23,51 @@ const TranslatorImage = (props: { job: any }) => {
     markerArea.show();
   };
 
-  const uploadEditedImage = async () => {
+  const handleSubmit = async () => {
     const imgToUpload = (
       document.getElementById('translator') as HTMLInputElement
     ).src;
-    const data = new FormData();
-    data.append('file', imgToUpload);
-    data.append('upload_preset', 'transl8rEdited');
 
-    // call to the api cloudinary need to be setup
-    const res = await fetch(
-      'https://api.cloudinary.com/v1_1/uro00/image/upload',
-      {
-        method: 'POST',
-        body: data,
-      },
-    );
-    const { secure_url } = await res.json();
+    const textToUpload = value;
 
-    uploadToDB(secure_url);
+    if (!(imgToUpload === imageUrl)) {
+      const data = new FormData();
+      data.append('file', imgToUpload);
+      data.append('upload_preset', 'transl8rEdited');
+
+      // call to the api cloudinary need to be setup
+      const res = await fetch(
+        'https://api.cloudinary.com/v1_1/uro00/image/upload',
+        {
+          method: 'POST',
+          body: data,
+        },
+      );
+      const { secure_url } = await res.json();
+
+      uploadToDB(secure_url, textToUpload);
+    } else {
+      uploadToDB('-', textToUpload);
+    }
   };
 
-  const uploadToDB = (secure_url: string) => {
-    //db magic
-    console.log(`${secure_url} in on db`);
+  const uploadToDB = (url: string, text: string) => {
+    //db magic + check "-"
+    console.log(`${url} and  ${text} in on db`);
     //rendering results if needed
     //setResultsImage(secure_url);
   };
 
+  const handleChange = (event: {
+    target: { value: React.SetStateAction<string> };
+  }) => {
+    setValue(event.target.value);
+  };
+
   return (
     <>
-      <h1>{jobName}</h1>
-      <div>
+      <h2>{jobName}</h2>
+      <form onSubmit={handleSubmit}>
         <img
           crossOrigin="anonymous"
           id="translator"
@@ -60,8 +76,15 @@ const TranslatorImage = (props: { job: any }) => {
           style={{ maxWidth: '100%' }}
           onClick={() => showMarkerArea()}
         />
-        <button onClick={() => uploadEditedImage()}>Send img</button>
-      </div>
+
+        <textarea
+          required
+          name="textarea"
+          value={value}
+          onChange={(event) => handleChange(event)}
+        />
+        <button type="submit">Send Translation</button>
+      </form>
 
       {/*
         <img src={resultsImage} alt="results" />
