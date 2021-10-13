226733141a88fa1c535e1a2aa9c1ad685d574934
Richard Barnes <rbarnes2@hotmail.co.uk>
2021-10-13 15:04:55 +0100

    if no jobs available for translator, code does not break

--- client/src/components/list/TranslatorJob.list.tsx
+++ client/src/components/list/TranslatorJob.list.tsx
@@ -28,15 +28,6 @@ const TranslatorJobList = (props: { jobs: any }) => {
     refetchInterval: 1000,
   });
 
-  const jobs = props.jobs;
-  const listJobs = jobs.map((job: any) => {
-    <li>
-      <TranslatorJobItem job={job} />
-    </li>;
-  });
-
-  <ul>{listJobs}</ul>;
-
   return (
     <div>
       <h2>Pending Jobs</h2>
@@ -44,9 +35,11 @@ const TranslatorJobList = (props: { jobs: any }) => {
       {status === 'loading' && <div>Fetching data</div>}
       {status === 'success' && (
         <div>
-          {data.map((job) => (
-            <TranslatorJobItem key={job._id} job={job} />
-          ))}
+          {data.length > 0 ? (
+            data.map((job) => <TranslatorJobItem key={job._id} job={job} />)
+          ) : (
+            <h3>No pending jobs</h3>
+          )}
         </div>
       )}
     </div>
