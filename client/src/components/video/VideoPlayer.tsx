eb8bc31e634e3a483fc0e2469a3e8c8ad4d862ad
Christian Letter <christianletter@gmail.com>
2021-10-16 14:06:14 +0200

    feat(videoplayer): button for calling the other user displays only when other user is connected

--- client/src/components/video/VideoPlayer.tsx
+++ client/src/components/video/VideoPlayer.tsx
@@ -16,7 +16,6 @@ socket.on('me', (id) => {
 });
 
 const VideoPlayer = () => {
-  const [idToCall, setIdToCall] = useState('');
   const [callAccepted, setCallAccepted] = useState<boolean>(false);
   const [callEnded, setCallEnded] = useState(false);
   const [stream, setStream] = useState();
@@ -41,11 +40,9 @@ const VideoPlayer = () => {
     });
     return await res.json();
   };
-  // const { data: dataFromDb, status } = useQuery('getSocketId', getSocketId, {
-  //   refetchInterval: 5000,
-  // });
+
   const queryResult = useQuery('getSocketId', getSocketId, {
-    refetchInterval: 5000,
+    refetchInterval: 1000,
   });
 
   const reqBody = { jobId: job.state._id, socketId: '' };
@@ -70,7 +67,7 @@ const VideoPlayer = () => {
       reqBody.socketId = socketId;
       setMe(socketId);
       insertToken();
-    }, 2000);
+    }, 500);
   };
 
   useEffect(() => {
@@ -90,78 +87,63 @@ const VideoPlayer = () => {
 
   const answerCall = () => {
     setCallAccepted(true);
-
     const peer = new Peer({ initiator: false, trickle: false, stream });
-
     peer.on('signal', (data) => {
       socket.emit('answerCall', { signal: data, to: call.from });
     });
-
     peer.on('stream', (currentStream) => {
       userVideo.current.srcObject = currentStream;
     });
-
     peer.signal(call.signal);
-
     connectionRef.current = peer;
   };
 
   const callUser = (id: any) => {
     const peer = new Peer({ initiator: true, trickle: false, stream });
-
     peer.on('signal', (data) => {
       socket.emit('callUser', { userToCall: id, signalData: data, from: me });
     });
-
     peer.on('stream', (currentStream) => {
       userVideo.current.srcObject = currentStream;
     });
-
     socket.on('callAccepted', (signal) => {
       setCallAccepted(true);
-
       peer.signal(signal);
     });
-
     connectionRef.current = peer;
   };
 
   const leaveCall = () => {
     setCallEnded(true);
-
     connectionRef.current.destroy();
-
     history.push('/dashboard');
   };
 
   return (
     <div>
-      {console.log('me', me)}
       {stream && <video playsInline muted ref={myVideo} autoPlay />}
       <div>
-        <CopyToClipboard text={me}>
-          <button type="button">Copy Your ID</button>
-        </CopyToClipboard>
-        <input
-          type="text"
-          placeholder="ID to call"
-          value={idToCall}
-          onChange={(e) => setIdToCall(e.target.value)}
-        />
+        {queryResult.data === undefined ||
+        queryResult.data.socketId === null ? (
+          <div>Waiting for the other user to connect</div>
+        ) : null}
+        {/* {console.log(queryResult)} */}
         {callAccepted && !callEnded ? (
           <button onClick={leaveCall}>Leave call</button>
-        ) : (
+        ) : null}
+        {console.log(queryResult.data)}
+        {queryResult.data !== undefined &&
+        queryResult.data.socketId !== null ? (
           <button onClick={() => callUser(queryResult.data.socketId)}>
             Call
           </button>
-        )}
-        {call.isReceivingCall && !callAccepted && (
+        ) : null}
+        {call.isReceivingCall && !callAccepted && queryResult.data !== null ? (
           <button type="button" onClick={answerCall}>
             Answer this call
           </button>
-        )}
+        ) : null}
       </div>
-
       {callAccepted && !callEnded && (
         <video playsInline ref={userVideo} autoPlay />
       )}
