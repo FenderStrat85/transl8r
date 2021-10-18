457dfdc30d01847d6e4e5f5d2b28999fe39039b3
Tommacfie <tommacfie@aol.com>
2021-10-18 19:43:39 +0200

    feat(dashboard): add logout and refactor jsx dashboard

--- client/src/screen/Dashboard.tsx
+++ client/src/screen/Dashboard.tsx
@@ -3,9 +3,11 @@ import TranslatorJobList from '../components/list/TranslatorJobList';
 import { UserContext } from '../context/Context';
 import { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
+import button from '../assets/icons/logout.svg';
 
 const Dashboard = (): JSX.Element => {
   const history = useHistory();
+  const logoutButton: string = button;
   const { user, logout } = useContext(UserContext);
   const accessToken: any = localStorage.getItem('accessToken');
 
@@ -28,28 +30,30 @@ const Dashboard = (): JSX.Element => {
         <>
 
           <div className='dashboard-screen__header'>
+            <img className='dashboard-screen__logout-button' src={logoutButton} alt='logout button' onClick={logoutFromApp} />
             <h1>Dashboard</h1>
           </div>
           <h2>Your Pending and Accepted jobs</h2>
           <CustomerJobList />
-          <button className='dashboard-screen__button' onClick={goToCompletedJobs}>
+          {/* <button className='dashboard-screen__button' onClick={goToCompletedJobs}>
             Take me to my completed jobs!
-          </button>
+          </button> */}
           <button className='dashboard-screen__button' onClick={toSelectJob}>Submit a different job</button>
-          <button className='dashboard-screen__button' onClick={logoutFromApp}>Logout</button>
+          {/* <button className='dashboard-screen__button' onClick={logoutFromApp}>Logout</button> */}
         </>
       ) : (
         <>
           <div className='dashboard-screen__header'>
+            <img className='dashboard-screen__logout-button' src={logoutButton} alt='logout button' onClick={logoutFromApp} />
             <h1>Dashboard</h1>
           </div>
           <TranslatorJobList />
-          <button className='dashboard-screen__button' onClick={goToCompletedJobs}>
-            Take me to my completed jobs!
-          </button>
-          <button className='dashboard-screen__button' onClick={logoutFromApp}>Logout</button>
+          {/* <button className='dashboard-screen__button' onClick={logoutFromApp}>Logout</button> */}
         </>
       )}
+      <button className='dashboard-screen__button' onClick={goToCompletedJobs}>
+        Take me to my completed jobs!
+      </button>
     </div>
   );
 };
