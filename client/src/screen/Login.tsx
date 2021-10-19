c30894c3e074578ca0e0df80501ced75479c0ccf
Richard Barnes <rbarnes2@hotmail.co.uk>
2021-10-19 20:25:55 +0100

    login page animation added

--- client/src/screen/Login.tsx
+++ client/src/screen/Login.tsx
@@ -1,7 +1,22 @@
+//@ts-nocheck
+import React, { useEffect, useRef } from 'react';
 import LoginForm from '../components/form/LoginForm';
 import { Link } from 'react-router-dom';
+import lottie from 'lottie-web';
 
 const LoginScreen = (): JSX.Element => {
+  const animationContainer = useRef(null);
+
+  useEffect(() => {
+    lottie.loadAnimation({
+      container: animationContainer.current,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      animationData: require('../assets/animations/login-page.json'),
+    });
+  }, []);
+
   return (
     <div className="login-screen">
       <h1>Transl8r</h1>
@@ -12,6 +27,7 @@ const LoginScreen = (): JSX.Element => {
           Register
         </button>
       </Link>
+      <div className="animationContainer" ref={animationContainer}></div>
     </div>
   );
 };
