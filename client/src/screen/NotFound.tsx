5eed6b0eebc36ca0f712738dc5c1682faf1345ab
Francesco Elia Foradori <francesco.elia.foradori@gmail.com>
2021-10-20 20:55:42 +0200

    heruku

--- client/src/screen/NotFound.tsx
+++ client/src/screen/NotFound.tsx
@@ -1,4 +1,20 @@
+import { useEffect } from 'react';
+import lottie from 'lottie-web';
+
 const NotFound = () => {
+  useEffect(() => {
+    lottie
+      .loadAnimation({
+        //@ts-expect-error
+        container: animationContainer.current,
+        renderer: 'svg',
+        loop: true,
+        autoplay: true,
+        animationData: require('../assets/animations/login-page.json'),
+      })
+      .setSpeed(0.35);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="not-found-screen">
       <h1>404 - Not Found</h1>
